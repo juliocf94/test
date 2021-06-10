9c6f635ab51ed637437758aff1c3b8de2d9ad714
juliocf94 <cesarff.julio@gmail.com>
2021-06-10 09:11:44 -0600

    primer commit a github

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Ejemplo de una web con PHP - aprenderaprogramar.com</title>
+
+<meta charset="utf-8">
+
+</head>
+
+<body>
+
+    <?php
+
+    /* Aquí vendría el código PHP */
+
+    /* Este código PHP podría ser una consulta a base de datos */
+
+    /* Y además podríamos mostrar esos datos */
+
+    ?>
+
+</body>
+
+</html>
